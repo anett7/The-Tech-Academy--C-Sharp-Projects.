f7dda88909be2a886f15b0242246885d1fdd56d0
anett7 <anette.kurucz@gmail.com>
2023-10-10 21:49:50 +0100

    part 4
    Part 4

--- Iteration/Program.cs
+++ Iteration/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Text;
 using System.Web;
 
 class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
         //int[]testScores = { 98, 99, 85, 70, 82, 34, 91, 90, 94};
 
         //for (int i=0; i< testScores.Length; i++)
@@ -67,27 +68,39 @@ class Program
         //Console.ReadLine();
 
 
-       // Console app assignment part 1
+        // Console app assignment part 1
         //create a one - dimensional Array of Strings
 
-        string[] sentence_start = { "My favorite color is: ", "I love eating: ", "My best friend is:" };
-        //ask user to input some text:
-        List<string> responses = new List<string>();
-        Console.WriteLine("Please enter your favorite color");
-        responses.Add(Console.ReadLine());
-        Console.WriteLine("Please enter a food you like");
-        responses.Add(Console.ReadLine());
-        Console.WriteLine("Please enter a friend's name that comes first in your mind");
-        responses.Add(Console.ReadLine());
+        //string[] sentence_start = { "My favorite color is: ", "I love eating: ", "My best friend is:" };
+        ////ask user to input some text:
+        //List<string> responses = new List<string>();
+        //Console.WriteLine("Please enter your favorite color");
+        //responses.Add(Console.ReadLine());
+        //Console.WriteLine("Please enter a food you like");
+        //responses.Add(Console.ReadLine());
+        //Console.WriteLine("Please enter a friend's name that comes first in your mind");
+        //responses.Add(Console.ReadLine());
 
-        //create a loop that goes through each string in the array, adding user's text to string and print this on separate lines.
-        for (; ; )//(int j = 0; j < sentence_start.Length; j++)
-        {
+        ////create a loop that goes through each string in the array, adding user's text to string and print this on separate lines.
+        //for (; ; )//(int j = 0; j < sentence_start.Length; j++)
+        //{
 
-            Console.WriteLine(sentence_start[j] + responses[j]);
-        }
-        Console.ReadLine();
+        //    Console.WriteLine(sentence_start[j] + responses[j]);
+        //}
+        //Console.ReadLine();
 
+        ////part 2
+        ////create an infinite loop, fix it
+        //bool control = true;
+        //while (control)
+        //{
+        //    Console.WriteLine("the control is true");
+
+
+        //    //update control variable to stop the infinite loop
+        //    control = false;
+        //    Console.ReadLine();
+        //}
         //Part 3
         //add a loop where the comparison that is used to determine whether to continue iterating
         // is a <+ opearator.
@@ -102,6 +115,61 @@ class Program
         //Console.WriteLine(ahh);
         //Console.ReadLine();
 
+        ////Part 4 create a list of strings where each item in the list is unique
+        //List<string> students = new List<string> { "Kiss", "Stein", "Nagy", "Kovacs", "Soos" };
+        ////ask the user to input text to search for in the list.
+        //Console.WriteLine("Enter student's name");
+
+        //string studentinput = Console.ReadLine();
+        //int index = 0;
+        //foreach (string item in students)
+        //{
+        //    if (studentinput == item)
+        //    {
+        //        Console.WriteLine("that is valid student name, it was found at index" + index);
+        //        index++;
+        //    }
+
+
+
+        //    else
+        //    {
+        //        Console.WriteLine("that is not a valid student name, please write a valid student name!");
+        //        index++;
+        //    }
+
+        //}
+        //Console.ReadLine();
+
+        //Part 4 create a list of strings where each item in the list is unique
+        List<string> students = new List<string> { "Kiss", "Stein", "Nagy", "Kovacs", "Soos" };
+        //ask the user to input text to search for in the list.
+        Console.WriteLine("Enter student's name");
+
+        string studentinput = Console.ReadLine();
+        for (int i = 0; i < students.Count; i++)
+        {
+            if (studentinput == students[i])
+            {
+                Console.WriteLine("that is a valid student name, it was found at index" + i);
+            }
+          
+            else
+             {
+              i++;
+             }
+        }
+        if (i == students.Count)
+        {
+
+            Console.WriteLine("that is not a valid student name, please write a valid student name!");
+
+        }
+        Console.ReadLine();
+        
+        
     }
+
+
 }
 

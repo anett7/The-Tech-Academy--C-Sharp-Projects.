d349b080ac1bcee9a1612bc6c77d7f82315ad5bb
anett7 <anette.kurucz@gmail.com>
2023-10-16 17:58:54 +0100

    Game 21
    first part
    creating classes
    developing objects in C#

---
+++ TwentyOne/Deck.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    public class Deck
+    {
+        public Deck() 
+        {
+            Cards = new List<Card>();
+            //Cards = new List<Card>();
+            //Card cardOne = new Card();
+            //cardOne.Face = "Two";
+            //cardOne.Suit = "Hearts";
+            //Cards.Add(cardOne);
+            //this is not the best solution to add one by one. 
+
+            //nested foreach loop:
+            //create two list and instanciate them with values Each of these items in the faces list
+            //we loop through 4 times. During each loop we create a card and assign the suit and face value
+            //then we add that new card we created in the our cards List.
+            List<string>Suits = new List<string>() { "Clubs", "Hearts", "Diamonds", "Spades"};
+
+            List<string> Faces = new List<string>() { "Two", "Three", "Four", "Five", "Six", "Seven",
+                                                    "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"};
+            //loop through both of these lists in order to generate a deck of 52 cards
+            foreach (string face in Faces)
+            {
+                foreach (string suit in Suits)
+                {
+                    Card card = new Card();
+                    card.Suit = suit;
+                    card.Face = face;
+                    //add this card to the list as above Cards = new List<Card>();
+                    Cards.Add(card);
+                    //once this loop ends it does not exist anymore that is why we can create it over and
+                    // over again with the same name.
+                }
+            }
+
+        }
+        public List<Card> Cards { get; set; }
+    }
+}
---
+++ TwentyOne/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Deck deck = new Deck();
+            //upon instanciating that object, it should have an entire deck of 52cards
+            //deck object which has a property cards, that cards list should have 52 cards in it
+            //to print it, we create an other foreach loop
+            foreach (Card card in deck.Cards)
+            {
+                Console.WriteLine(card.Face + " of " +card.Suit);
+
+            }
+
+            //print out count to see it is 52:
+            Console.WriteLine(deck.Cards.Count);
+
+            //to fill up deck one by one START would be like this:
+
+            //Deck deck = new Deck();
+            //deck.Cards = new List<Card>();
+
+            //Card cardOne = new Card();
+            //cardOne.Face = "Queen";
+            //cardOne.Suit = "Spades";
+            //deck.Cards.Add(cardOne);
+
+            //fill up one by one END. Lets create a constractor instead.
+
+            //Console.WriteLine(cardOne.Face + " of " + cardOne.Suit);
+            Console.ReadLine();
+        }
+    }
+}

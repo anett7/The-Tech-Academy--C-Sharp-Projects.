fa818d39fa4ae514ece8828731a06859ec86521e
anett7 <anette.kurucz@gmail.com>
2023-10-27 15:10:10 +0100

    Method assignment final
    calling method, parameters specified by name.

--- Method_Assignment/Program.cs
+++ Method_Assignment/Program.cs
@@ -12,8 +12,10 @@ namespace Method_Assignment
         {
             //instantiate the class
             _2intParameters math= new _2intParameters();
-            //call the method passing in 2 numbers
-            Console.WriteLine(math.Math2Int(6, 3));
+            //call the method in the class passing in 2 numbers
+            //Console.WriteLine(math.Math2Int(6, 3));
+            //call the method in the class specifying the parameters by name
+            Console.WriteLine(math.Math2Int(num2: 3, num1: 6));
             Console.ReadLine();
 
         }

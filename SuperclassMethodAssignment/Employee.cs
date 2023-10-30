78ab61aee4d1880db362e893b55da6acc97e7900
anett7 <anette.kurucz@gmail.com>
2023-10-30 14:39:08 +0000

    superclass
    Inheritance classes

---
+++ SuperclassMethodAssignment/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperclassMethodAssignment
+{
+    //create another class Employee and have it inherit from the Person class
+    public class Employee : Person
+    {
+        //give the Employee class a property called ID and have it be of data type int.
+        public int Id { get; set; }
+
+    }
+}
---
+++ SuperclassMethodAssignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperclassMethodAssignment
+{
+     class Program
+    {
+        static void Main(string[] args)
+            // Inside main method instantiate and initialize an Employee object with first name:sample last name:student
+        {
+            Employee employee = new Employee() { FirstName = "Sample", LastName = "Student" };
+            //Call the superclass method SayName() on the Employee object
+            employee.SayName();
+            Console.ReadLine();
+
+        }
+    }
+}

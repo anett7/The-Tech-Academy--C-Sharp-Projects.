c518a03dccf23aeee2acad4431fbce114d8a29af
anett7 <anette.kurucz@gmail.com>
2023-11-12 17:53:45 +0000

    Polymorphism assignment
    interface

---
+++ SuperclassMethodAssignment/IQuittable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperclassMethodAssignment
+{// create an intervace callled IQuittable and have it define a void method called Quit().
+    interface IQuittable
+    {
+        void Quit(Employee employee);
+    }
+}
--- SuperclassMethodAssignment/Person.cs
+++ SuperclassMethodAssignment/Person.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace SuperclassMethodAssignment
 {
-    public class Person
+    public abstract class Person
     {
         //create a class called Person and give it two properties, each of data type string
         //one called FirstName, the other LastName
@@ -15,11 +15,7 @@ namespace SuperclassMethodAssignment
 
         //give the class a void method called SayName() that takes no parameters and simply writes
         //the person's full name to the console in the format of "Name:[full name]"
-        public void SayName()
-        {
+        public abstract void SayName();
 
-            Console.WriteLine("Name: " + this.FirstName + " "+ this.LastName);
-
-        }
     }
 }

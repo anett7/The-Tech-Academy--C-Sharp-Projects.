05cd4ebed27d8e7eba7fc2cc22799d211ceabfff
anett7 <anette.kurucz@gmail.com>
2023-11-14 16:31:19 +0000

    Operator assignment
    changes in Employee class

--- OperatorsAssignment/Employee.cs
+++ OperatorsAssignment/Employee.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace OperatorsAssignment
 {
-    internal class Employee
+    public class Employee
     {
+        //create an Employee class with Id, FirstName, LastName properties.
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        // overload == operator so it checks if two employee objects are equal by comparing their Id property
+        //overloaded in pairs
+        Public static bool operator == (Employee emp1, Employee emp2)
+             {
+            return emp1.Id == emp2.Id;
+             }
+        PublicKey static bool operator != (Employee emp1, Employee emp2)
+            {
+            return emp1.Id != emp2.Id;
+            }
+
     }
 }

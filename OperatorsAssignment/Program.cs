b6343d72ec0b280117fdcd91f48534f93225c050
anett7 <anette.kurucz@gmail.com>
2023-11-14 16:28:08 +0000

    OperatorAss.
    compare 2 employee object

--- OperatorsAssignment/Program.cs
+++ OperatorsAssignment/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,7 +10,14 @@ namespace OperatorsAssignment
     internal class Program
     {
         static void Main(string[] args)
+            //createing employee objects
         {
+            Employee emp1 = new Employee() { FirstName = "Lilly", LastName= "Stein"};
+            Employee emp2 = new Employee() { FirstName = "Ben", LastName = "Kiss" };
+           emp1.Id =1;
+           emp2.Id = 2;
+
+
         }
     }
 }

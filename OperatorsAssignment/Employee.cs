87b09626f8f2b9d1a59fb97af02d089ecea0158f
anett7 <anette.kurucz@gmail.com>
2023-11-14 20:23:56 +0000

    Operator Assign.
    changes

--- OperatorsAssignment/Employee.cs
+++ OperatorsAssignment/Employee.cs
@@ -14,11 +14,12 @@ namespace OperatorsAssignment
         public string LastName { get; set; }
         // overload == operator so it checks if two employee objects are equal by comparing their Id property
         //overloaded in pairs
-        Public static bool operator == (Employee emp1, Employee emp2)
+
+        Public static bool operator ==(Employee emp1, Employee emp2)
              {
             return emp1.Id == emp2.Id;
              }
-        PublicKey static bool operator != (Employee emp1, Employee emp2)
+        Public static bool operator != (Employee emp1, Employee emp2)
             {
             return emp1.Id != emp2.Id;
             }

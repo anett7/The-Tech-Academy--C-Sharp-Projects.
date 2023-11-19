304fbfc1aeb91a4b2a6b9117ebfbb6f0299841ba
anett7 <anette.kurucz@gmail.com>
2023-11-19 22:41:47 +0000

    21Game
    putting it all together 1-2

---
+++ 21Game/Player.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21Game
+{
+    public class Player
+    {
+        //constructor, takes 2 parameters and assign some values to these
+        public Player(string name, int beginningBalance)
+        {
+            //create an empty list card, we need to initalise list, otherwise we can't add things to them           
+            Hand = new List<Card>();
+            //we assign balance
+            Balance = beginningBalance;
+            //we assign name
+            Name = name;
+        }//we created a constructor that takes 2 arguments, we are taking those arguments and we assinging them
+        //to properties in the class
+        public List<Card> Hand { get; set; }
+        public int Balance { get; set; }
+        public string Name { get; set; }
+        public bool isActivelyPlaying { get; set; }
+        //this is a property very specific to 21Game, better to create a 21Player class that inherit from Player class
+        //we keep this inside of Player class for now:
+        public bool Stay { get; set; }
+        public static Game operator +(Game game, Player player)
+        {
+            game.Players.Add(player);
+            retrun game;
+        }
+        public static Game operator -(Game game, Player player)
+            {
+            game.Players.Remove(player);
+            return Game;
+            }
+    }
+}
---
+++ 21Game/_21Game.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21Game
+{
+    public class _21Game :Game, IWalkAway
+    {
+        //dealer is specific to the 21Game, therfore we have this dealer property
+        public TwentyOneDealer Dealer { get; set; }
+
+        //implementing play method, it is an override method, an abstract method in game class, which means
+        //we have to have an implementation of it
+        public override void Play()
+        {
+            //when we start game.play in Program class, we already have a game going and we instantiate
+            //that dealer object as a new 21 dealer
+            Dealer =new TwentyOneDealer();
+            
+            //since we are in a while loop, we are not sure at what stage we are at. We want to reset
+            //all the players. game.Play is going to play one hand and the while loop goes through in program class
+            //then an other hand, as long as the player says, that he is actively playing and balance is not zero
+
+            //Reset the player. When we perform an action on a player, we actually want to do that on all the players
+            //as in Game class we have a list of players not just one
+            foreach(Player player in Players)
+        }
+            
+    }//we need to instantiate our dealer
+
+
+}

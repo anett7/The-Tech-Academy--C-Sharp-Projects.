664e72554569050ddc75a12e7d8f4bfa57563c5e
anett7 <anette.kurucz@gmail.com>
2024-02-20 16:22:19 +0000

    Newsletter
    MVC Sample Application Newsletter

--- NewsletterAppMVC/Controllers/HomeController.cs
+++ NewsletterAppMVC/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,7 +13,36 @@ namespace NewsletterAppMVC.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public ActionResult SignUp(string firstName, string lastName, string emailAddress)
 
+        {
+            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName) || string.IsNullOrEmpty(emailAddress)) 
+            {
+                return View("~/Views/Shared/Error.cshtml");
+            }
+            else
+            {
+                string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Newsletter;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+                string queryString = @"INSERT INTO dbo.Table (FirstName, LastName, EmailAddress) VALUES (@FirstName, @LastName, @EmailAddress)";
+                using(SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    SqlCommand command = new SqlCommand(queryString, connection);
+                    command.Parameters.Add("@FirstName", System.Data.SqlDbType.VarChar);
+                    command.Parameters.Add("@LastName", System.Data.SqlDbType.VarChar);
+                    command.Parameters.Add("@EmailAddress", System.Data.SqlDbType.VarChar);
+
+                    command.Parameters["@FirstName"].Value = firstName;
+                    command.Parameters["@LastName"].Value = lastName;
+                    command.Parameters["@EmailAddress"].Value = emailAddress;
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                                    }
+                return View("Success");
+            }
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";

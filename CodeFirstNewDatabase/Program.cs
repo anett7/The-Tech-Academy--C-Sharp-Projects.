f0220e5f23cf8eac29154a3535ffa5c5762fb59c
anett7 <anette.kurucz@gmail.com>
2024-03-18 19:45:05 +0000

    CodeFirst
    basic Entity Framework Code-First Console application that creates a basic Student database and adds one Student.

---
+++ CodeFirstNewDatabase/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstNewDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new SchoolContext())
+            {
+
+                var stud = new Student() { StudentName = "Billy" };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+
+            }
+        }
+    }
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+    }
+    public class Grade
+    {
+        public int GradeId { get; set; }
+        public string GradeName { get; set; }
+        public string Section { get; set; }
+        public ICollection<Student> Students { get; set; }
+    }
+
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base()
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+    }
+}
